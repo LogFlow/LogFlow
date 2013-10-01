216830cec7f7f0f04f345b7982ae763a5a1d0ede
Roger Wilson <ctjinx@gmail.com>
2013-10-01 10:03:36 +0200

    Fixed failing test

--- source/LogFlow.Specifications/Helpers/EmptyInput.cs
+++ source/LogFlow.Specifications/Helpers/EmptyInput.cs
@@ -1,16 +1,10 @@
-﻿using System;
-
-namespace LogFlow.Specifications.Helpers
+﻿namespace LogFlow.Specifications.Helpers
 {
 	public class EmptyInput : ILogInput
 	{
-		public void StartReading(Func<Result, bool> processResult)
-		{
-			processResult(new Result());
-		}
-
 		public void Start(FluentLogContext logContext, Result result)
 		{
+			logContext.TryRunProcesses(result);
 		}
 
 		public void Stop()
