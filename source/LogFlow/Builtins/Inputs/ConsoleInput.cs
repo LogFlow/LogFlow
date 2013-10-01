be2cf9040743366c9179cbca5240e29b8eb72aba
Roger Wilson <ctjinx@gmail.com>
2013-10-01 08:45:12 +0200

    First draft of ConsoleInput

---
+++ source/LogFlow/Builtins/Inputs/ConsoleInput.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LogFlow.Builtins.Inputs
+{
+	public class ConsoleInput : ILogInput
+	{
+		private readonly CancellationTokenSource _tokenSource;
+		private readonly CancellationToken _token;
+
+		public ConsoleInput()
+		{
+			_tokenSource = new CancellationTokenSource();
+			_token = _tokenSource.Token;
+		}
+
+		public void Start(FluentLogContext logContext, Result result)
+		{
+			Task.Factory.StartNew(() =>
+				{
+					while (true)
+					{
+
+
+						if (_token.IsCancellationRequested)
+						{
+							break;
+						}
+					}
+				}, _token);
+		}
+
+		public void Stop()
+		{
+			_tokenSource.Cancel();
+		}
+	}
+}
