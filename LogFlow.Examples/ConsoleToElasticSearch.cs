af60954adc9b6c1a336552f0e72fa21d75e2d398
Roger Wilson <ctjinx@gmail.com>
2013-10-01 10:17:02 +0200

    Added console to elasticsearch

---
+++ LogFlow.Examples/ConsoleToElasticSearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LogFlow.Builtins.Inputs;
+using LogFlow.Builtins.Outputs;
+
+namespace LogFlow.Examples
+{
+	public class ConsoleToElasticSearch : LogFlow
+	{
+		public ConsoleToElasticSearch()
+		{
+			Console.WriteLine("ConsoleToElasticSearch constructor is beeing run.");
+			CreateProcess("C2ES", new ConsoleInput())
+				.AddProcess(new ElasticSearchOutput(new ElasticSearchConfiguration()));
+		}
+	}
+}
--- LogFlow.Examples/FileToConsoleLogFlow.cs
+++ LogFlow.Examples/FileToConsoleLogFlow.cs
@@ -4,13 +4,13 @@ using LogFlow.Builtins.Outputs;
 
 namespace LogFlow.Examples
 {
-	public class FileToConsoleLogFlow : LogFlow
-	{
-		public FileToConsoleLogFlow()
-		{
-			Console.WriteLine("FileToConsoleLogFlow constructor is beeing run.");
-			CreateProcess("fileToFlow", new FileInput(@"C:\Users\1323\Desktop\NLogTryout\NLogTryout\*.txt"))
-				.AddProcess(new LineToConsoleOutput());
-		}
-	}
+	//public class FileToConsoleLogFlow : LogFlow
+	//{
+	//	public FileToConsoleLogFlow()
+	//	{
+	//		Console.WriteLine("FileToConsoleLogFlow constructor is beeing run.");
+	//		CreateProcess("fileToFlow", new FileInput(@"C:\Users\1323\Desktop\NLogTryout\NLogTryout\*.txt"))
+	//			.AddProcess(new LineToConsoleOutput());
+	//	}
+	//}
 }
