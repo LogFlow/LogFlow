38e8dee51b915a488d0f849754bd71968f964366
Roger Wilson <ctjinx@gmail.com>
2013-10-01 11:13:03 +0200

    Enabled file to console again

--- LogFlow.Examples/FileToConsoleLogFlow.cs
+++ LogFlow.Examples/FileToConsoleLogFlow.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using System.IO;
 using LogFlow.Builtins.Inputs;
 using LogFlow.Builtins.Outputs;
 
 namespace LogFlow.Examples
 {
-	//public class FileToConsoleLogFlow : LogFlow
-	//{
-	//	public FileToConsoleLogFlow()
-	//	{
-	//		Console.WriteLine("FileToConsoleLogFlow constructor is beeing run.");
-	//		CreateProcess("fileToFlow", new FileInput(@"C:\Users\1323\Desktop\NLogTryout\NLogTryout\*.txt"))
-	//			.AddProcess(new LineToConsoleOutput());
-	//	}
-	//}
+	public class FileToConsoleLogFlow : LogFlow
+	{
+		public FileToConsoleLogFlow()
+		{
+			Console.WriteLine("FileToConsoleLogFlow started for: '{0}'", GetFilePath());
+
+			CreateProcess("fileToFlow", new FileInput(GetFilePath()))
+				.AddProcess(new LineToConsoleOutput());
+		}
+
+		private string GetFilePath()
+		{
+			return Path.Combine(Directory.GetCurrentDirectory(), "*.txt");
+		}
+	}
 }
