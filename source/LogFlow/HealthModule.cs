8627d9df46d60b7d4ee9c128cd5a4043982f9e5d
Emil Cardell <emil.cardell@thomascook.se>
2013-10-01 15:03:35 +0200

    Added missing file

---
+++ source/LogFlow/HealthModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using System.Linq;
+
+namespace LogFlow
+{
+	public class HealthModule : NancyModule
+	{
+		public HealthModule()
+		{
+			Get["/"] = _ =>
+			{
+				var flows = LogFlowEngine.FlowBuilder.Flows.Select(f => new { f.LogType, f.IsBroken, f.BrokenStart, Processes = f.Processes.Count });
+				return Response.AsJson(flows);
+			};
+		}
+	}
+}
