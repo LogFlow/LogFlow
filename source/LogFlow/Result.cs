457195cd85a6fca64929c13ef4dd2ad68b308d12
Roger Wilson <ctjinx@gmail.com>
2013-10-04 09:17:56 +0200

    Added metadata to Result

--- source/LogFlow/Result.cs
+++ source/LogFlow/Result.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 
 namespace LogFlow
@@ -10,11 +11,13 @@ namespace LogFlow
 			Id = Guid.NewGuid();
 			Json = new JObject();
 			Canceled = false;
+			MetaData = new Dictionary<string, string>();
 		}
 
 		public Guid Id { get; private set; }
 		public string Line { get; set; }
 		public JObject Json { get; set; }
 		public bool Canceled { get; set; }
+		public Dictionary<string, string> MetaData { get; private set; }
 	}
 }
