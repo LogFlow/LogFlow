80791bab6858b8268a2a5315da0e380d8876abd6
Emil Cardell <emil.cardell@gmail.com>
2014-10-25 23:57:13 +0200

    Forgot to add File

---
+++ source/LogFlow/JSonKeys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogFlow
+{
+	public class JSonKeys
+	{
+		public const string LogType = "logType";
+		public const string MachineName = "machineName";
+	}
+}
